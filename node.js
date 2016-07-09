5dbcbc60beb31ea8433f0be472470bf5897650b9
Milan Pal Singh <milanpalsingh89@gmail.com>
2016-07-09 14:52:36 -0700

    setup

---
+++ node.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.use(express.static('files'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!');
+});
