f38404adc10a7b83a709a308e1e95a809d8b97c9
Charles M. Chong <cchong.vise@gmail.com>
2016-07-09 21:43:05 -0700

    Update node.js to support mongoose driver
    Added MongoDB functionality to node.js
    Adding dependency packages to package.json

--- node.js
+++ node.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var app = express();
 
+var mongoose = require('mongoose');
+
+// Mongoose connection to MongoDB
+mongoose.connect('mongodb://localhost/mongo_fix', function (error) {
+    if (error) {
+        console.log(error);
+    }
+});
+
 app.use(express.static('public'));
 app.use(express.static('files'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.redirect('Home.html');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
 });
+
